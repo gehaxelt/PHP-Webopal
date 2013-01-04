097ba22a397d1ce8d5cf814455968334ecebd5f1
Sebastian Gehaxelt <github@gehaxelt.in>
2013-01-04 10:09:24 +0100

    Commented logout.php

--- inc/logout.php
+++ inc/logout.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+/**
+Simple logout script.
+Destroy and unset the current session and redirect the user to the main page.
+**/
 session_unset();
 session_destroy();
 header("Location: ../index.php");
